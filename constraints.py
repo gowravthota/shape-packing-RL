a1042825a562c89ff42e8d53ecd42e2254202ebe
Thota, Gowrav <gthota6@gatech.edu>
2025-06-15 04:03:32 -0400

    Create constraints.py

---
+++ constraints.py
@@ -0,0 +1,28 @@
+import math
+
+class Shape:
+    def __init__(self, x, y, orientation=0, size=10, color="blue"):
+        self.x = x
+        self.y = y
+        self.orientation = orientation
+        self.size = size
+        self.color = color
+
+    def create_rectangle(self, canvas):
+        pass
+
+    def create_circle(self, canvas):
+        pass
+
+    def create_triangle(self, canvas):
+        pass
+
+    def create_polygon(self, canvas):
+        pass
+
+
+
+
+
+
+
